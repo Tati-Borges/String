8b7924562b0408f8d795ee56d5bec42361266d04
Tati-Borges <paula.tatiele@escola.pr.gov.br>
2025-08-13 09:01:49 -0300

    Add files via upload

---
+++ Strings/extrator-de-palavras-chave-aula2/script.js
@@ -0,0 +1,11 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('entrada-de-texto').value;
+    const campoResultor = document.querySelector('resultado-palavrachave');
+    const Palavras = texto.split("");
+
+    campoResultado.textContent = Palavras.join(", ");
+}
